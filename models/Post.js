95db276813073320e2d3bdb7f3373c2e1832250f
darylbg <darylxcuf@gmail.com>
2023-05-09 20:11:01 +0100

    added seed data for User Post Comment models

--- models/Post.js
+++ models/Post.js
@@ -1,5 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection,js');
+const sequelize = require('../config/connection.js');
 
 class Post extends Model {}
 
