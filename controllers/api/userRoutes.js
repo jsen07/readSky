ab679b4a8186df4add3a068bc94daf11fd17c74f
unknown <jayssendecastro@gmail.com>
2023-05-10 02:34:48 +0100

    registration completed

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -4,6 +4,8 @@ const { User } = require('../../models');
 router.post('/', async (req, res) => {
     try {
         const userData = await User.create({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
             username: req.body.username,
             email: req.body.email,
             password: req.body.password,
@@ -14,6 +16,18 @@ router.post('/', async (req, res) => {
             res.status(200).json(userData);
         });
     } catch (err) {
+        console.log(err);
         res.status(500).json(err);
     }
 })
+// router.get('/', async (req, res) => {
+//     try {
+//         const userData = await User.findAll();
+//         const users = userData.map((user) => user.get({ plain: true }))
+//         res.status(200).json(users);
+//     } catch (error) {
+//         res.status(500).json(error);
+//     }
+// });
+
+module.exports = router;
