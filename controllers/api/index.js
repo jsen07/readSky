669f3af9137586a81c99c2d0d24cc080a9a85624
darylbg <darylxcuf@gmail.com>
2023-05-15 18:58:46 +0100

    updated index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,17 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
+
 const searchRoutes = require('./searchRoutes');
 const commentRoutes = require('./commentRoutes');
 
+
+
+
+const profileRoutes = require('./profileRoutes');
+
+router.use('/profile', profileRoutes);
+router.use('/users', userRoutes);
 
 router.use('/post', postRoutes);
 router.use('/search', searchRoutes);
