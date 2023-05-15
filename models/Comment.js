db665c969d884c131d53bb888671eba779cfa9ec
Harun Saini <harunsaini@Haruns-MacBook-Pro.local>
2023-05-15 20:00:05 +0100

    adding comments to models/index.js and comment.js

--- models/Comment.js
+++ models/Comment.js
@@ -1,10 +1,13 @@
+// The code snippet imports the necessary modules from the 'sequelize' library for working with database models in Node.js.
 const { Model, DataTypes } = require('sequelize');
+// The code snippet requires the 'sequelize' module from the '../config/connection.js' file, indicating the establishment of a connection to a database using Sequelize in a Node.js application.
 const sequelize = require('../config/connection.js');
-
+// The code snippet defines a Comment class that extends the Model class. This suggests that the Comment class is a model class used for interacting with a specific data table or entity in a database.
 class Comment extends Model {}
 
 Comment.init(
     {
+        // The code snippet defines an id attribute for the Comment model. This id attribute is of type INTEGER and serves as the primary key for the Comment model. It is configured to auto-increment, meaning that its value will automatically increase for each new record inserted into the database.
         id: {
             type: DataTypes.INTEGER,
             primaryKey: true,
@@ -35,12 +38,13 @@ Comment.init(
         }
     },
     {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'comment'
+        // This code snippet provides configuration options for the Comment model.
+        sequelize, //sequelize indicates the Sequelize instance that is used for this model. 
+        timestamps: false, //timestamps: false suggests that the Comment model does not include automatic timestamp fields like createdAt and updatedAt.
+        freezeTableName: true, // freezeTableName: true indicates that the table name for the Comment model should be the same as the model name, preventing Sequelize from automatically pluralizing the table name.
+        underscored: true, // underscored: true specifies that the column names in the Comment model should use underscored naming convention (e.g., created_at instead of createdAt).
+        modelName: 'comment' // modelName: 'comment' sets the model name to 'comment', which will be used by Sequelize for various operations.
     }
 );
-
+// The code snippet exports the Comment model, making it available for other parts of the application to use. This allows other files or modules to import and utilize the Comment model for performing database operations related to comments.
 module.exports = Comment;
--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
-const User = require('./User.js');
-const Post = require('./Post.js');
-const Comment = require('./Comment.js')
+const User = require('./User.js'); // The code snippet imports the User model from the './User.js' file. This suggests that the User model is defined in a separate module is needed for performing database operations related to users in a Node.js application.
+
+const Post = require('./Post.js'); // The code snippet imports the Post model from the './Post.js' file. This indicates that the Post model is defined in a separate module or file and is required for performing database operations related to posts in a Node.js application.
+
+const Comment = require('./Comment.js') // The code snippet imports the Comment model from the './Comment.js' file. This suggests that the Comment model is defined in a separate module or file and is needed for performing database operations related to comments in a Node.js application.
 
 //User can have many posts
 User.hasMany(Post, {
@@ -36,4 +38,4 @@ Comment.belongsTo(User, {
 });
 
 
-module.exports = { User, Post, Comment }
+module.exports = { User, Post, Comment } // The code snippet exports the User, Post, and Comment models as an object. This allows other parts of the application to import all three models from a single module.
