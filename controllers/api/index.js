492a88b5bb66b37f476616f8e1d0feec9e7baa94
darylbg <darylxcuf@gmail.com>
2023-05-15 18:54:33 +0100

    debugged comment route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,9 +4,10 @@ const postRoutes = require('./postRoutes');
 const searchRoutes = require('./searchRoutes');
 const commentRoutes = require('./commentRoutes');
 
-router.use('/users', userRoutes);
+
 router.use('/post', postRoutes);
 router.use('/search', searchRoutes);
 router.use('/comment', commentRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
